a5f3822c8ae19ee3bdc5d0275df3ab32210afc6c
Darshani <dbarasker@systango.com>
2022-10-14 16:58:29 +0530

    Added Changes for google-map integration

--- src/app/component/checkout/checkout.component.ts
+++ src/app/component/checkout/checkout.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { PaymentComponent } from '../payment/payment.component';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit ,  AfterViewInit{
+  @ViewChild('map',{ static: true }) mapElement: any;
+  map: google.maps.Map={} as any;
+
   checkoutForm:FormGroup={} as any;
   constructor(private dialog:MatDialog) { }
+
+  ngAfterViewInit(): void {
+    
+  }
 
   ngOnInit(): void {
     this.checkoutForm=new FormGroup({
@@ -19,7 +27,19 @@ export class CheckoutComponent implements OnInit {
       'address':new FormControl('',Validators.required),
       'city':new FormControl('',Validators.required),
       'postalPin':new FormControl('',[Validators.required,Validators.pattern('[0-9]*')]),
-    })
+    });
+    const mapProperties = {
+      center: new google.maps.LatLng(22.7196,  75.8577),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    google.maps.event.addDomListener(window, 'load', this.initialize);
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+  }
+  initialize()
+  {
+    var input = document.getElementById('location') as any;
+    var autocomplete = new google.maps.places.Autocomplete(input);
   }
   getErrorMessage()
   {
@@ -37,6 +57,7 @@ export class CheckoutComponent implements OnInit {
     }
     return '';
   }
+ 
   OnCheckout()
   {
     if(!this.checkoutForm.untouched)
@@ -54,3 +75,5 @@ export class CheckoutComponent implements OnInit {
     }
   }
 }
+// https://angular-material-extensions.github.io/google-maps-autocomplete/doc/index.html
+// https://medium.com/@dhormale/use-google-places-api-autocomplete-using-angular-for-resident-and-office-address-23cc33078e8
