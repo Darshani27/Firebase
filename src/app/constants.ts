b183ba043727b7fc2c245de667869718fba39e7e
Darshani <dbarasker@systango.com>
2022-12-07 18:14:06 +0530

    added changes for constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,15 +1,23 @@
 export class CONSTANTS
 {
-    public static readonly LONG_TEXT_KEY ='answer';
-    public static readonly BOOL_KEY='bool';
-    public static readonly MULTI_SELECT_KEY='response';
-    public static readonly SINGLE_SELECT_KEY='response';
-    public static readonly SHORT_TEXT_KEY='answer';
-    public static readonly LONG_TEXT='LONG_TEXT';
     public static readonly BOOL='BOOL';
     public static readonly SHORT_TEXT='SHORT_TEXT';
+    public static readonly LONG_TEXT='SHORT_TEXT';
     public static readonly MULTI_SELECT='MULTI_SELECT';
     public static readonly SINGLE_SELECT='SINGLE_SELECT';
+}
+
+export enum KeyEnum
+{
+    LONG_TEXT_KEY='answer',
+    MULTI_SELECT_KEY='response',
+    SINGLE_SELECT_KEY='response',
+    BOOL_KEY='bool',
+
+
+
+
+
 }
 // https://github.com/transistorsoft/capacitor-background-fetch
 // https://github.com/capawesome-team/capacitor-background-task
